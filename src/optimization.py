52625c893d51a6d18287da5f413912f9f00dc381
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-10-04 08:08:42 -0400

    updated optimization.py

--- src/optimization.py
+++ src/optimization.py
@@ -10,7 +10,8 @@ def get_loss():
     """
 
     # YOUR CODE HERE: select a loss appropriate for classification
-    loss  = # YOUR CODE HERE
+    loss  = nn.CrossEntropyLoss()
+
 
     return loss
 
@@ -36,7 +37,10 @@ def get_optimizer(
         # optimizer. Use the input parameters learning_rate, momentum
         # and weight_decay
         opt = torch.optim.SGD(
-            # YOUR CODE HERE
+            model.parameters(),
+            lr=learning_rate,
+            momentum=momentum,
+            weight_decay=weight_decay,
         )
 
     elif optimizer.lower() == "adam":
@@ -44,7 +48,9 @@ def get_optimizer(
         # optimizer. Use the input parameters learning_rate, momentum
         # and weight_decay
         opt = torch.optim.Adam(
-            # YOUR CODE HERE
+            model.parameters(),
+            lr=learning_rate,
+            weight_decay=weight_decay,
         )
     else:
         raise ValueError(f"Optimizer {optimizer} not supported")
